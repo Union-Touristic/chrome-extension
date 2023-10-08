264a45f3b74625867ac5c5a249b41f2d8999d7e9
Alibek Massalimov <alibekmslv@gmail.com>
2023-10-08 18:36:36 +0300

    Get rid off Root.tsx

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import Root from './Root';
+import Providers from './context/providers';
+import ExtensionApp from './ExtensionApp';
 import './index.css';
 
 const container = document.getElementById('app-container');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<Root />);
+root.render(
+  <Providers>
+    <ExtensionApp />
+  </Providers>
+);
